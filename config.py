ffea327c01d13b4001e72e963121d133758abb62
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2023-01-24 19:54:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 import os
 
 
-class Config(object):
+class config(object):
 	API_ID = int(os.environ.get("API_ID", "0"))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
 	CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100"))
-	SUPPORT_GROUP = os.environ.get("UPDATES_CHANNEL", "")
-	SUPPORT_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+	SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "")
+	SUPPORT_CHANNEL = os.environ.get("SUPPORT_CHANNEL", "")
         BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
         SHORTNER_SITE = os.environ.get("SHORTNER_SITE", "")
         API_KEY = os.environ.get("API_KEY", "")
