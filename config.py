1e4859c853075ab0bf1ac4ced15da778aac8e547
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2023-01-24 20:32:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 
 
 class config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100"))
-	SUPPORT_GROUP = os.environ.get("SUPPORT_CHAT", "")
-	SUPPORT_CHANNEL = os.environ.get("SUPPORT_CHANNEL", "")
+	API_ID = int(os.environ.get("API_ID", "6435225"))
+	API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5951446975:AAHomxCgmzt2HAyLF5K5E3nvoZGihYywY4Y")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "PyroStorageBot")
+	CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001851293267"))
+	SUPPORT_GROUP = os.environ.get("SUPPORT_CHAT", "TheDeadlyBots")
+	SUPPORT_CHANNEL = os.environ.get("SUPPORT_CHANNEL", "TheBotUpdates")
         BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
-        SHORTNER_SITE = os.environ.get("SHORTNER_SITE", "")
-        API_KEY = os.environ.get("API_KEY", "")
+        SHORTNER_SITE = os.environ.get("SHORTNER_SITE", "https://easysky.in")
+        API_KEY = os.environ.get("API_KEY", "8644d61bb75d25519b6b0acdec99bf369fa1cbdd")
