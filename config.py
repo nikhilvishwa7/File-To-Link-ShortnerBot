cad30a7f10af96caa289ac7f6a11a654ee62d60d
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-08-17 17:04:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ class config(object):
 	CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001932233757"))
 	SUPPORT_GROUP = os.environ.get("SUPPORT_CHAT", "NewMovie1stOnTG")
 	SUPPORT_CHANNEL = os.environ.get("SUPPORT_CHANNEL", "bot_channel_011")
-        BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
+        BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", '1234567890')).split()
         SHORTNER_SITE = os.environ.get("SHORTNER_SITE", "https://TinyFy.in")
         API_KEY = os.environ.get("API_KEY", "8f29e4130ed3439c4c5cfc5c56dae86c0aff0daa")
